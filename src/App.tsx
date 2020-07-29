c90805192c672a27acf1c9b99a12fe81005f8d1f
Stefan Solopa <stefan.solopa@gmail.com>
2020-07-29 14:51:34 +0300

    Updated deploy

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,20 @@ import 'antd/dist/antd.css';
 import Layout from "antd/lib/layout";
 import Home from './pages/home/Home'
 
-const { Header, Content } = Layout;
+const {Header, Content} = Layout;
 
 function App() {
-  return (
-    <Layout style={{ height: '100vh' }}>
-      <Header >
-       <h1 className="text-white" >Stock Price - Alpha Vantage</h1>
-      </Header>
+    return (
+        <Layout style={{height: '100vh'}}>
+            <Header>
+                <h1 className="text-white">Stock Price - Alpha Vantage</h1>
+            </Header>
 
-        <Content style={{ margin: 40, height: '100%' }}>
-          <Home />
-        </Content>
-
-    </Layout>
-  );
+            <Content style={{margin: 40, height: '100%'}}>
+                <Home/>
+            </Content>
+        </Layout>
+    );
 }
 
 export default App;
