7e25d3f758cd90d69a4d8c7d69fb9313b23aaafa
Stefan Solopa <stefan.solopa@gmail.com>
2020-07-29 15:23:28 +0300

    Small fix

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'build')));
+  app.use(express.static(path.join(__dirname, './build')));
 
   app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, '../build', 'index.html'));
+    res.sendFile(path.join(__dirname, './build', 'index.html'));
   });
 }
 
